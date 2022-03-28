d9b89ecea9a8206e5ab125af3416563e071f5e38
emersonps <emersonline2007@gmail.com>
2022-03-27 23:00:39 -0300

    Prepação para ambiente do Back end 1. Java SE instalado e  2. Em seguida foi configurada a variável de ambiente para que o Mavin, Arow possam encontrar essas variáveis no sistema. 3. Foi realizado a instalação e configuração da variável de ambiente do Apache Mavim, para gerenciamento de tarefas as dependências do projeto, e, também, utilizado para fazer o bulding do sistema. 4.Utilização dos pacotes: Spring Web; Lombok; Spting Data JPA; H2 DataBase; Spring Boot Dev Tools;

--- crud-spring/src/main/java/com/emerson/controller/StudentController.java
+++ crud-spring/src/main/java/com/emerson/controller/StudentController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import lombok.AllArgsConstructor;
 
 @RestController
