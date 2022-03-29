57ab3b03cb88dd4a6de87ad0f47d906aa792ea99
emersonps <emersonline2007@gmail.com>
2022-03-29 00:04:32 -0300

    Adiçao de linhas na tabela

--- crud-spring/src/main/java/com/emerson/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/emerson/CrudSpringApplication.java
@@ -26,16 +26,37 @@ public class CrudSpringApplication {
 			Schedule s = new Schedule();
 			s.setTeacher("Emerson Pinheiro");
 			s.setCourse("HTML");
-			s.setTime("10/04/2022 10h00 à 20h00");
+			s.setTime("10/04/2022 17h00 às 20h00");
 
 			scheduleRepository.save(s);
 
 			Schedule s1 = new Schedule();
 			s1.setTeacher("Emerson Pinheiro");
 			s1.setCourse("CSS");
-			s1.setTime("10/05/2022 10h00 à 20h00");
+			s1.setTime("10/05/2022 10h00 àa 12h00");
 
 			scheduleRepository.save(s1);
+
+			Schedule s2 = new Schedule();
+			s2.setTeacher("Maria Pinheiro");
+			s2.setCourse("JS");
+			s2.setTime("04/05/2022 11h00 às 12h00");
+
+			scheduleRepository.save(s2);
+
+			Schedule s3 = new Schedule();
+			s3.setTeacher("Welleson Gama");
+			s3.setCourse("JQuery");
+			s3.setTime("04/04/2022 10h00 à 13h00");
+
+			scheduleRepository.save(s3);
+
+			Schedule s4 = new Schedule();
+			s4.setTeacher("Alessandro Henrique");
+			s4.setCourse("PHP");
+			s4.setTime("04/04/2022 10h00 à 13h00");
+
+			scheduleRepository.save(s4);
 		};
 	}
 }
